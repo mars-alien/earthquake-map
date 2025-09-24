c0812257bb7cc80c57cae1ddd78caa2ec9b0f379
Royal <royalsachaniiitm@gmail.com>
2025-09-24 16:32:31 +0530

    Update URL to fetch earthquake data for the last hour

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -2,14 +2,14 @@ import requests
 import pandas as pd
 import os
 
-# Create data folder if not exists
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 os.makedirs(DATA_DIR, exist_ok=True)
 CSV_PATH = os.path.join(DATA_DIR, "earthquakes.csv")
 
 # USGS API for past 7 days
-URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv"
+URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv"
 
 def fetch_earthquake_data():
     r = requests.get(URL)
